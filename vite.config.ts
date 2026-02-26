083d20cf2987157ca5ca7634c9226f8283588f70
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2026-02-26 20:30:13 +0100

    revert back to nitro

--- vite.config.ts
+++ vite.config.ts
@@ -7,54 +7,13 @@ import { fileURLToPath, URL } from 'url'
 
 import tailwindcss from '@tailwindcss/vite'
 import contentCollections from '@content-collections/vite'
-
-const clientOnlyPackages = [
-  '@radix-ui/react-accordion',
-  '@radix-ui/react-alert-dialog',
-  '@radix-ui/react-avatar',
-  '@radix-ui/react-checkbox',
-  '@radix-ui/react-dialog',
-  '@radix-ui/react-collapsible',
-  '@radix-ui/react-context-menu',
-  '@radix-ui/react-dropdown-menu',
-  '@radix-ui/react-form',
-  '@radix-ui/react-label',
-  '@radix-ui/react-hover-card',
-  '@radix-ui/react-menubar',
-  '@radix-ui/react-one-time-password-field',
-  '@radix-ui/react-navigation-menu',
-  '@radix-ui/react-popover',
-  '@radix-ui/react-password-toggle-field',
-  '@radix-ui/react-portal',
-  '@radix-ui/react-progress',
-  '@radix-ui/react-radio-group',
-  '@radix-ui/react-scroll-area',
-  '@radix-ui/react-select',
-  '@radix-ui/react-slider',
-  '@radix-ui/react-switch',
-  '@radix-ui/react-tabs',
-  '@radix-ui/react-toast',
-  '@radix-ui/react-toggle',
-  '@radix-ui/react-toolbar',
-  '@radix-ui/react-toggle-group',
-  '@radix-ui/react-tooltip',
-  '@radix-ui/react-collection',
-  '@radix-ui/react-presence',
-  '@radix-ui/react-dismissable-layer',
-  '@radix-ui/react-focus-guards',
-  '@radix-ui/react-menu',
-  '@radix-ui/react-focus-scope',
-  '@radix-ui/react-roving-focus',
-]
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  },
-  ssr: {
-    noExternal: clientOnlyPackages,
   },
   plugins: [
     devtools(),
@@ -64,12 +23,8 @@ const config = defineConfig({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
-    tanstackStart({
-      prerender: {
-        enabled: true,
-        crawlLinks: true,
-      },
-    }),
+    nitro(),
+    tanstackStart(),
     viteReact(),
   ],
 })
